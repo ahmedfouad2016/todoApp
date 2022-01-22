6c0eded2910d009187f35449a27f9cae77ed6f95
Ahmed Fouad <ahmed.fouad@istnetworks.com>
2022-01-22 05:43:57 +0300

    feat: add persistor

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+
 import {AppNavigation} from 'navigation';
 import {Splash} from 'containers/Splash';
-import {store} from 'store';
+import {store, persistedStore} from 'store';
 
 const App: React.FC<{}> = () => {
   // return <Splash />;
 
   return (
     <Provider store={store}>
-      <AppNavigation />
+      <PersistGate loading={null} persistor={persistedStore}>
+        <AppNavigation />
+      </PersistGate>
     </Provider>
   );
 };
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,42 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {configureStore, combineReducers} from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import listReducer from './List/ListSlice';
 
-export const store = configureStore({
-  reducer: {
-    counter: listReducer,
-  },
+const rootReducer = combineReducers({
+  lists: listReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage: AsyncStorage,
+  blacklist: ['navigation'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: __DEV__,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistedStore = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
